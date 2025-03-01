3ea5e01c36e7898acc9791dd0b3303e3458e1097
ратяяя <148002321+qerranii@users.noreply.github.com>
2025-03-01 23:39:26 +0300

    login

--- final_project/final_project_app/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.core.validators import validate_comma_separated_integer_list
-
-# Create your models here.
-
-class LoginForm(forms.Form):
-    login = forms.CharField(label='Login', max_length=100)
-    password = forms.CharField(label='Password', max_length=25)
