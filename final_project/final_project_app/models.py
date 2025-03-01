b8f24fb4a423a8320c404dc5af6738d01db9a565
Dennis Efremov <defremov689@gmail.com>
2025-03-01 23:49:19 +0300

    1/2 of registration done

--- final_project/final_project_app/models.py
+++ final_project/final_project_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    height = models.IntegerField(max_length=3)
+    weight = models.IntegerField(max_length=3)
+    chest = models.IntegerField(max_length=3)
+    waist = models.IntegerField(max_length=3)
+    hips = models.IntegerField(max_length=3)
+    about_me = models.CharField()
 
 # Create your models here.
--- final_project/final_project_app/views.py
+++ final_project/final_project_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from final_project_app.models import User
 # Create your views here.
 
 def index_page(request):
@@ -12,6 +12,25 @@ def log_in_page(request):
 
 def sign_up_page(request):
     context = {}
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password1']
+        password2 = request.POST['password2']
+        height = request.POST['height']
+        weight = request.POST['weight']
+        chest = request.POST['chest']
+        waist = request.POST['waist']
+        hips = request.POST['hips']
+        if password == password2:
+            user = User.objects.create_user(username, email, password, height, weight, chest, waist, hips)
+            try:
+                user.save()
+                return redirect('/profile')
+            except:
+                raise NotImplementedError
+        else:
+            raise NotImplementedError
     return render(request, "auth/sign_up.html", context)
 
 def profile_page(request):
